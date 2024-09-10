77a7e5b6dfa01d80c5319f049758e63ab54e62f7
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-10 06:18:40 +0300

    create injection.dart provide & integrate & trigger cubit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,15 +1,59 @@
+import 'package:api_project/cubit/my_cubit.dart';
+import 'package:api_project/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<User> usersList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<MyCubit>(context).emitGetAllUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeScreen'),
+        title: const Text('HomeScreen'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          BlocBuilder<MyCubit, MyState>(
+            builder: (BuildContext context, state) {
+              if (state is GetAllUsers) {
+                usersList = (state).allUsersList;
+                return ListView.builder(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(8),
+                  itemCount: usersList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 50,
+                      color: Colors.amber,
+                      child: Center(
+                        child: Text(
+                          usersList[index].name.toString(),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }else{
+                return const Center(child: CircularProgressIndicator(),);
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/injection.dart
@@ -0,0 +1,29 @@
+import 'package:api_project/cubit/my_cubit.dart';
+import 'package:api_project/repos/my_repo.dart';
+import 'package:api_project/services/web_services.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void initGetIt() {
+  getIt.registerLazySingleton<MyCubit>(() => MyCubit(getIt()));
+  getIt.registerLazySingleton<MyRepo>(() => MyRepo(getIt()));
+  getIt.registerLazySingleton<WebServices>(() => WebServices(createAndSetupDio()));
+}
+
+Dio createAndSetupDio() {
+  Dio dio = Dio();
+  dio
+    ..options.connectTimeout = (1 * 1000) as Duration?
+    ..options.receiveTimeout = (1 * 1000) as Duration?;
+  dio.interceptors.add(LogInterceptor(
+    requestBody: true,
+    error: true,
+    requestHeader: false,
+    responseHeader: false,
+    request: true,
+    responseBody: true,
+  ));
+  return dio;
+}
