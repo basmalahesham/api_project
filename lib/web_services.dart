32331f304b0e7ed16a3866df9b6a369ccf8db2dc
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-10 04:08:09 +0300

    create web_services.dart

---
+++ lib/web_services.dart
@@ -0,0 +1,10 @@
+import 'package:api_project/models/users.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+part 'web_services.g.dart';
+@RestApi(baseUrl: 'https://gorest.co.in/public/v2/')
+abstract class WebServices {
+  factory WebServices(Dio dio, {String? baseUrl}) = _WebServices;
+  @GET('users')
+  Future<List<Users>> getAllUsers();
+}
