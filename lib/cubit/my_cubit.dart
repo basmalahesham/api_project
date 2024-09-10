4374b7afddfb425324f8c374af676ea54402c849
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-10 04:37:38 +0300

    create my_cubit.dart & my_state.dart

---
+++ lib/cubit/my_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:api_project/models/user.dart';
+import 'package:api_project/repos/my_repo.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'my_state.dart';
+
+class MyCubit extends Cubit<MyState> {
+  final MyRepo myRepo;
+  MyCubit(this.myRepo) : super(MyInitial());
+  void emitGetAllUsers() {
+    myRepo.getAllUsers().then((usersList) {
+      emit(GetAllUsers(usersList));
+    });
+  }
+}
