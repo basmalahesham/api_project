701b4c5798cba104c8cd0c5a83503042d5377d52
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-12 03:52:57 +0300

    add post request

--- lib/services/web_services.dart
+++ lib/services/web_services.dart
@@ -2,6 +2,7 @@ import 'package:api_project/models/user.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 part 'web_services.g.dart';
+
 @RestApi(baseUrl: 'https://gorest.co.in/public/v2/')
 abstract class WebServices {
   factory WebServices(Dio dio, {String? baseUrl}) = _WebServices;
@@ -11,4 +12,8 @@ abstract class WebServices {
 
   @GET('users/{id}')
   Future<User> getUserById(@Path('id') int userId);
+
+  @POST('users')
+  Future<User> createNewUser(
+      @Body() User newUser, @Header('Authorization') String token);
 }
