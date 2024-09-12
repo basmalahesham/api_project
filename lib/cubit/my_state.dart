3bcd449ca8abed2827b507bd11cb439660a46079
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-12 04:33:11 +0300

    create deleteUser state & cubit

--- lib/cubit/my_state.dart
+++ lib/cubit/my_state.dart
@@ -4,19 +4,27 @@ part of 'my_cubit.dart';
 sealed class MyState {}
 
 final class MyInitial extends MyState {}
+
 final class GetAllUsers extends MyState {
   final List<User> allUsersList;
 
   GetAllUsers(this.allUsersList);
 }
+
 final class GetUserDetails extends MyState {
   final User userDetails;
 
   GetUserDetails(this.userDetails);
 }
+
 final class CreateNewUser extends MyState {
   final User newUser;
 
   CreateNewUser(this.newUser);
 }
 
+final class DeleteUser extends MyState {
+  final dynamic data;
+
+  DeleteUser(this.data);
+}
