7fab259df16c97fca7b48ef60ed3a7385e0a54cd
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-12 04:01:03 +0300

    create createNewUser fun in my_repo.dart

--- lib/repos/my_repo.dart
+++ lib/repos/my_repo.dart
@@ -7,16 +7,21 @@ class MyRepo {
   MyRepo(this.webServices);
 
   Future<List<User>> getAllUsers() async {
-    var response = await webServices.getAllUsers();
-    // singleUserDataFromJson = one object from list
-    return response
-        .map((user) => User.fromJson(user.toJson()))
-        .toList(); // هنا عشان ال returnType نوعه list of object فلازم اللى راجع احوله لليست
+    //var response = await webServices.getAllUsers();
+    /*// singleUserDataFromJson = one object from list
+    return response.map((user) => User.fromJson(user.toJson())).toList(); // هنا عشان ال returnType نوعه list of object فلازم اللى راجع احوله لليست
+    */
+    //return response;
+    return await webServices.getAllUsers();
   }
 
   Future<User> getUserById(int userId) async {
-    var response = await webServices.getUserById(userId);
-    return User.fromJson(response
-        .toJson()); //  هنا عشان ال returnType نوعه  object فلازم اللى راجع يبقا اوبجيكت
+    /*var response = await webServices.getUserById(userId);
+    return User.fromJson(response.toJson()); //  هنا عشان ال returnType نوعه  object فلازم اللى راجع يبقا اوبجيكت*/
+    return await webServices.getUserById(userId);
+  }
+
+  Future<User> createNewUser(User newUser)async{
+    return await webServices.createNewUser(newUser, 'Bearer 05e9bb5f38505c0dd8116f54db9308a5d4e290eab2d350d30689c29810479a6f');
   }
 }
